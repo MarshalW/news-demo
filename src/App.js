6fdc9a4dff29fcdf973417d39f2c404ccf885a38
Marshal <marshal.wu@qq.com>
2016-02-24 16:35:18 +0800

    simplest ajax async action

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 'use strict';
 import React, { Component,PropTypes } from 'react';
 import { connect } from 'react-redux';
+import request from 'superagent';
 import NewsBar from './NewsBar';
 import NewsList from './NewsList';
 
-export const RECEIVE_POSTS = 'RECEIVE_POSTS';
-
-function refreshNewsList(){
+function refreshNewsList(news){
   return {
-    type:'refreshNewsList'
+    type:'refreshNewsList',
+    state:news
   };
 }
 
 function refreshNewsListAsync(){
   return (dispatch,getState)=>{
-    return setTimeout(()=>{
-      dispatch(refreshNewsList());
-    },100);    
+    return request
+      .get('/news.json')
+      .set('Accept', 'application/json')
+      .end(function(err, res){
+        let news=JSON.parse(res.text);
+        dispatch(refreshNewsList(news));
+    });    
   };
 }
 
--- src/index.js
+++ src/index.js
@@ -4,32 +4,11 @@ import { render } from 'react-dom';
 import { createStore,applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
-import request from 'superagent';
 import App from './App';
 
-const news=[
-	{
-		id:'a1',
-		title:'沪指震荡收跌0.8%，险守2900点'
-	},
-	{
-		id:'b1',
-		title:'上海中心城区中小户型不得低于七成'
-	},
-	{
-		id:'a14',
-		title:'德国加入俄罗斯高铁争夺 中俄莫喀高铁合作出现变数'
-	},
-	{
-		id:'ab1',
-		title:'多家上市公司发布退市预警 净利润亏损成主因'
-	}
-];
-
 let reducers=function(state={news:[]},action){
-	console.log('>>>reducers:'+action);
 	if(action.type=='refreshNewsList'){
-		return {news:news};	
+		return {news:action.state.news};	
 	}else{
 		return state;
 	}
@@ -47,10 +26,3 @@ render(
 	document.getElementById('root')
 );
 
-request
-  .get('/news.json')
-  .set('Accept', 'application/json')
-  .end(function(err, res){
-    console.log('err: '+err);
-    console.log(res);
-  });
